8d1ea628e79b1cf81b0bd80e6568753a1b920872
ibecir <becir@bioptimizers.com>
2026-05-15 10:33:29 +0200

    Restructure

--- app/src/main/java/com/example/todo/di/FirebaseModule.kt
+++ app/src/main/java/com/example/todo/di/FirebaseModule.kt
@@ -4,6 +4,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.persistentCacheSettings
 import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
